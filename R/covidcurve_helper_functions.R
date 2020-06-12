4f335852830e892c213d4e8a970197992cac4c3c
Nicholas Brazeau <nbrazeau@med.unc.edu>
2020-06-12 17:58:00 -0400

    add simulation work

---
+++ R/covidcurve_helper_functions.R
@@ -0,0 +1,74 @@
+source("R/assertions_v5.R")
+library(tidyverse)
+library(stringr)
+
+#' @title Make IFR Uniform Distributed Reparameterized Param Df
+#' @param num_mas positive interger; Number of IFR strata to infer
+
+make_ma_reparamdf <- function(num_mas = 10) {
+  assert_pos_int(num_mas)
+  tibble::tibble(name = paste0("ma", 1:num_mas),
+                 min  = rep(0, num_mas),
+                 init = rep(0.5, num_mas),
+                 max = rep(1, num_mas),
+                 dsc1 = rep(0, num_mas),
+                 dsc2 = rep(1, num_mas))
+}
+
+
+#' @title Make Spline Y Position Reparameterized Param Df
+#' @param num_ys positive interger; Number of Spline Y positions to infer
+#' @param max_yvec vector; Describes the ymax position. Rest will be uniform 0,1 for reparameterized positions
+
+make_spliney_reparamdf <- function(max_yvec = list("name" = "y3", min = 0, init = 9, max = 14, dsc1 = 0, dsc2 = 14),
+                              num_ys = 5) {
+  assert_pos_int(num_ys)
+  assert_in(names(max_yvec), c("name", "min", "init", "max", "dsc1", "dsc2"))
+  assert_in(c("name", "min", "init", "max", "dsc1", "dsc2"), names(max_yvec))
+  assert_string(max_yvec[["name"]])
+  assert_numeric(max_yvec[["min"]])
+  assert_numeric(max_yvec[["init"]])
+  assert_numeric(max_yvec[["max"]])
+  assert_numeric(max_yvec[["dsc1"]])
+  assert_numeric(max_yvec[["dsc2"]])
+
+  out <- tibble::tibble(name = paste0("y", 1:num_ys),
+                        min  = rep(0, num_ys),
+                        init = rep(0.5, num_ys),
+                        max = rep(1, num_ys),
+                        dsc1 = rep(0, num_ys),
+                        dsc2 = rep(1, num_ys))
+  out %>%
+    dplyr::filter(name != max_yvec["name"]) %>%
+    dplyr::bind_rows(., max_yvec) %>%
+    dplyr::arrange(name)
+}
+
+#' @title Make Spline X (Knots) Position Reparameterized Param Df
+#' @param num_xs positive interger; Number of Spline X positions to infer
+#' @param max_xvec vector; Describes the ymax position. Rest will be uniform 0,1 for reparameterized positions
+
+make_splinex_reparamdf <- function(max_xvec = list("name" = "x4", min = 180, init = 190, max = 200, dsc1 = 180, dsc2 = 200),
+                                   num_xs = 4) {
+  assert_pos_int(num_xs)
+  assert_in(names(max_xvec), c("name", "min", "init", "max", "dsc1", "dsc2"))
+  assert_in(c("name", "min", "init", "max", "dsc1", "dsc2"), names(max_xvec))
+  assert_string(max_xvec[["name"]])
+  assert_numeric(max_xvec[["min"]])
+  assert_numeric(max_xvec[["init"]])
+  assert_numeric(max_xvec[["max"]])
+  assert_numeric(max_xvec[["dsc1"]])
+  assert_numeric(max_xvec[["dsc2"]])
+
+  out <- tibble::tibble(name = paste0("x", 1:num_xs),
+                        min  = rep(0, num_xs),
+                        init = rep(0.5, num_xs),
+                        max = rep(1, num_xs),
+                        dsc1 = rep(0, num_xs),
+                        dsc2 = rep(1, num_xs))
+  out %>%
+    dplyr::filter(name != max_xvec["name"]) %>%
+    dplyr::bind_rows(., max_xvec) %>%
+    dplyr::arrange(name)
+}
+
--- R/simple_seir_model.R
+++ R/simple_seir_model.R
@@ -1,25 +1,42 @@
-library(odin)
-#' no export
-seir_step <- function(S, E, I, R, N, Beta, mu_EI, mu_IR, mu_L, delta.t){
-  SE_events <- rbinom(n = 1, size = S, prob = 1 - exp((-mu_L -Beta*(I/N))*delta.t))
-  SE_deaths <- rbinom(n = 1, size = SE_events, prob = 1 - exp(-mu_L/(mu_L +Beta*(I/N))))
-  infected <- SE_events - SE_deaths
-  EI_events <- rbinom(n = 1, size = E, prob = 1 - exp(-mu_L -mu_EI*delta.t))
-  EI_deaths <- rbinom(n = 1, size = EI_events, prob = 1 - exp(-mu_L/(mu_L + mu_EI)))
-  infectious <- EI_events - EI_deaths
-  IR_events <- rbinom(n = 1, size = I, prob = 1 - exp(-mu_L -mu_IR*delta.t))
-  IR_deaths <- rbinom(n = 1, size = IR_events, prob = 1 - exp(-mu_L/(mu_L + mu_IR)))
-  recovered <- IR_events - IR_deaths
-  recovered_deaths <- rbinom(n = 1, size = R, prob = 1 - exp(-mu_L))
+simple_seir <- odin::odin({
+  ## deal with time
+  dt <- user(4) # Specified timestep
+  time <- step * dt # Tracking actual time
+  ## Core equations for transitions between compartments:
+  update(S) <- S - n_SE
+  update(E) <- E + n_SE - n_EI
+  update(I) <- I + n_EI - n_IR
+  update(R) <- R + n_IR
 
-  S <- S - infected + (EI_deaths + IR_deaths + recovered_deaths)
-  E <- E + infected - infectious - EI_deaths
-  I <- I + infectious - recovered - IR_deaths
-  R <- R + recovered - recovered_deaths
+  ## Individual probabilities of transition:
+  p_SE <- 1 - exp(-beta * (I / N) * dt)
+  p_EI <- 1 - exp(-sigma * dt)
+  p_IR <- 1 - exp(-gamma * dt)
+
+  ## Draws from binomial distributions for numbers changing between
+  ## compartments:
+  n_SE <- rbinom(S, p_SE)
+  n_EI <- rbinom(E, p_EI)
+  n_IR <- rbinom(I, p_IR)
+
+  ## Total population size
   N <- S + E + I + R
-  return(c("N" = N, "S" = S, "E" = E, "I" = I, "R" = R))
-}
 
+  ## Initial states:
+  initial(S) <- S_ini
+  initial(E) <- E_ini
+  initial(I) <- I_ini
+  initial(R) <- R_ini
+
+  ## User defined parameters - default in parentheses:
+  S_ini <- user(1000)
+  E_ini <- user(1)
+  I_ini <- user(1)
+  R_ini <- user(0)
+  beta <- user(0.2)
+  gamma <- user(0.1)
+  sigma <- user(0.1)
+})
 
 
 #' @title Simple SEIR Model
@@ -28,62 +45,65 @@ seir_step <- function(S, E, I, R, N, Beta, mu_EI, mu_IR, mu_L, delta.t){
 #' @param R0 numeric; Number of individuals immune at time 0
 #' @param betas numeric vector; beta values corresponding to beta change time points
 #' @param beta_change numeric vector; times that correspond to beta changes in the \link{betas} vector
-#' @param mu_EI numeric; rate of transition from E-I compartment: (1/duration of incubation/latency period)
-#' @param mu_IR numeric; rate of transition from I-R compartment: (1/duration of disease period)
-#' @param mu_L numeric; birth/death rate: (1/life expectancy)
+#' @param sigma numeric; rate of transition from E-I compartment: (1/duration of incubation/latency period)
+#' @param gamma numeric; rate of transition from I-R compartment: (1/duration of disease period)
 #' @param time numeric; total time of epidemic observed, up to but not including this "day"
 
-run_simple_seir <-  function(N, E0, R0, betas, mu_EI, mu_IR, mu_L, time, ...){
+run_simple_seir <-  function(N, E0, R0, betas, beta_changes, sigma, gamma, time, ...){
   source("R/assertions_v5.R")
   assert_numeric(N)
   assert_numeric(E0)
   assert_numeric(R0)
   assert_numeric(betas)
   assert_numeric(beta_changes)
-  assert_numeric(mu_EI)
-  assert_numeric(mu_IR)
-  assert_numeric(mu_L)
+  assert_numeric(sigma)
+  assert_numeric(gamma)
   assert_numeric(time)
   assert_greq(time, max(beta_changes))
 
   #......................
   # setup
   #......................
-  S0 <- N - E0 - R0
-  beta_changes_steps <- c((beta_changes - dplyr::lag(beta_changes))[2:length(beta_changes)],
-                          (time - beta_changes[length(beta_changes)]))
-  betas <- rep(betas, times = beta_changes_steps)
-  time.steps <- 1:(time-1)
-
-
-  # init
-  ret <- matrix(NA, nrow = length(time.steps) , ncol = 5)
-  ret[1,] <- seir_step(S = S0,
-                       E = E0,
-                       I = 0,
-                       R = R0,
-                       N = N,
-                       Beta = betas[1],
-                       mu_EI = mu_EI,
-                       mu_IR = mu_IR,
-                       mu_L = mu_L,
-                       delta.t = 0)
-
-  for (t in 2:length(time.steps)) {
-    ret[t,] <-  seir_step(S = ret[t-1, 2],
-                          E = ret[t-1, 3],
-                          I = ret[t-1, 4],
-                          R = ret[t-1, 5],
-                          N = ret[t-1, 1],
-                          Beta = betas[t],
-                          mu_EI = mu_EI,
-                          mu_IR = mu_IR,
-                          mu_L = mu_L,
-                          delta.t = time.steps[t])
+  if (length(beta_changes) > 1) {
+    beta_changes_steps <- c(1, (beta_changes - dplyr::lag(beta_changes))[2:length(beta_changes)],
+                            (time - beta_changes[length(beta_changes)]))
+  } else {
+    beta_changes_steps <- c(1, time-1)
   }
-  # out
-  ret <- cbind.data.frame(time, ret)
-  colnames(ret) <- c("time", "N", "S", "E", "I", "R")
-  return(ret)
 
+  beta_changes <- c(beta_changes, time)
+  # init empty
+  ret <- matrix(NA, nrow = time, ncol = 5)
+  # loop through
+  mod.prev <- simple_seir(
+    beta = betas[1],
+    sigma = sigma,
+    gamma = gamma,
+    S_ini = N,
+    E_ini = E0,
+    I_ini = 0,
+    R_ini = 0)
+  mod.prev <- mod.prev$run(1)[1,]
+
+  for (i in 1:length(betas)) {
+    mod.new <- simple_seir(
+      beta = betas[i],
+      sigma = sigma,
+      gamma = gamma,
+      S_ini = mod.prev["S"],
+      E_ini = mod.prev["E"],
+      I_ini = mod.prev["I"],
+      R_ini = mod.prev["R"])
+    mod.new <- mod.new$run(1:(beta_changes_steps[i+1]+1)) # new beta, "new start"
+    # store
+    ret[beta_changes[i]:beta_changes[i+1], ] <- mod.new
+    # overwrite prev
+    mod.prev <- mod.new[nrow(mod.new),]
+  }
+  # clean up "time"
+  ret <- as.data.frame(ret)
+  colnames(ret) <- colnames(mod.new)
+  ret$step <- 1:nrow(ret)
+  return(ret)
 }
+
