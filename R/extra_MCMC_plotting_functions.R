d0700d759bae5c2b8b693d93f5698667febdfab0
Nicholas Brazeau <nbrazeau@med.unc.edu>
2020-07-03 13:48:29 -0400

    update mcmc quick plot to include serorate

--- R/extra_MCMC_plotting_functions.R
+++ R/extra_MCMC_plotting_functions.R
@@ -12,16 +12,17 @@ quick_mc_diagnostics <- function(modout) {
   maxmachain <- drjacoby::plot_par(modout$mcmcout, maxma, display = FALSE)
   spechain <- drjacoby::plot_par(modout$mcmcout, "spec", display = FALSE)
   serodaychain <- drjacoby::plot_par(modout$mcmcout, "sero_day", display = FALSE)
+  seroratechain <- drjacoby::plot_par(modout$mcmcout, "sero_rate", display = FALSE)
 
   maxmachain <- maxmachain[[1]][["trace"]] + theme(legend.position = "none")
   spechain <- spechain[[1]][["trace"]] + theme(legend.position = "none")
-  serodaychain <- serodaychain[[1]][["trace"]] + theme(legend.position = "bottom")
-
+  serodaychain <- serodaychain[[1]][["trace"]] + theme(legend.position = "none")
+  seroratechain <- seroratechain[[1]][["trace"]] + theme(legend.position = "bottom")
   # out
   lftside <- cowplot::plot_grid(mcaccplot, mcacclogplot1, mcacclogplot2,
                                 nrow = 3)
-  rightside <- cowplot::plot_grid(maxmachain, spechain, serodaychain,
-                                  nrow = 3, rel_heights = c(1,1,1.5))
+  rightside <- cowplot::plot_grid(maxmachain, spechain, serodaychain, seroratechain,
+                                  nrow = 4, rel_heights = c(1,1,1,1.5))
   cowplot::plot_grid(lftside, rightside, ncol = 2)
 
 }
