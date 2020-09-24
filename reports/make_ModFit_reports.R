56b0bd95641bc793e68bb2e5d7b4b9befe62ba15
nickbrazeau <nbrazeau1@gmail.com>
2020-09-23 23:49:26 -0400

    update reports

--- reports/make_ModFit_reports.R
+++ reports/make_ModFit_reports.R
@@ -8,10 +8,9 @@ paths <- list.files("results/Modfits_noserorev/", full.names = TRUE)
 #......................
 renderMyDocument <- function(path) {
   countrychar <- stringr::str_split(basename(path), "_", simplify = T)[[1]]
-  stratachar <- ifelse(grepl("age", basename(path)), "Age-Band", "Region")
   rmarkdown::render("reports/Modfit_report_base.Rmd",
                     params = list( path = path ),
-                    output_file = paste0(here::here(), "/reports/", countrychar, "-", stratachar, "_NoSeroRev_report.pdf"))
+                    output_file = paste0(here::here(), "/reports/", countrychar, "-", "_NoSeroRev_report.pdf"))
 }
 
 lapply(paths, renderMyDocument)
@@ -28,10 +27,9 @@ paths <- list.files("results/Modfits_serorev/", full.names = TRUE)
 #......................
 renderMyDocument <- function(path) {
   countrychar <- stringr::str_split(basename(path), "_", simplify = T)[[1]]
-  stratachar <- ifelse(grepl("age", basename(path)), "Age-Band", "Region")
   rmarkdown::render("reports/Modfit_report_base.Rmd",
                     params = list( path = path ),
-                    output_file = paste0(here::here(), "/reports/", countrychar, "-", stratachar, "_SeroRev_report.pdf"))
+                    output_file = paste0(here::here(), "/reports/", countrychar, "-", "_SeroRev_report.pdf"))
 }
 
 lapply(paths, renderMyDocument)
@@ -46,10 +44,9 @@ paths <- list.files("results/Modfits_carehomes/", full.names = TRUE)
 #......................
 renderMyDocument <- function(path) {
   countrychar <- stringr::str_split(basename(path), "_", simplify = T)[[1]]
-  stratachar <- ifelse(grepl("age", basename(path)), "Age-Band", "Region")
   rmarkdown::render("reports/Modfit_report_base.Rmd",
                     params = list( path = path ),
-                    output_file = paste0(here::here(), "/reports/", countrychar, "-", stratachar, "_CareHomes_report.pdf"))
+                    output_file = paste0(here::here(), "/reports/", countrychar, "-", "_CareHomes_report.pdf"))
 }
 
 lapply(paths, renderMyDocument)
