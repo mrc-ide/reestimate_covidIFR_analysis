8d2ee0aa7718a511bccc421d9e4bf526b5903981
nickbrazeau <nbrazeau1@gmail.com>
2020-09-24 00:39:43 -0400

    update collate sims

--- analyses/07-collate_sims.R
+++ analyses/07-collate_sims.R
@@ -10,25 +10,17 @@ source("R/my_themes.R")
 #............................................................
 # lightweight function to make plot
 #...........................................................
-get_sim_IFR_curve <- function(sim_param_map, modout) {
-  trueseroprev <- sim_param_map %>%
-    dplyr::pull("simdat")
-  trueseroprev <- trueseroprev[[1]]$StrataAgg_Seroprev
-
-
+get_sim_IFR_curve <- function(curve, modout) {
+  # pull out  true curve
+  truecurve <- tibble::tibble(time = 1:length(curve),
+                              truecurve = curve)
+  # pull out inferred infection curve
   infxncurve <- COVIDCurve::draw_posterior_infxn_cubic_splines(IFRmodel_inf = modout,
                                                                dwnsmpl = 1e2,
                                                                by_chain = TRUE,
-                                                               by_strata = FALSE)$curvedata
-  infxncurve %>%
-    dplyr::select(c("chain", "sim", "time", "totinfxns")) %>%
-    ggplot() +
-    geom_line(aes(x = time, y = totinfxns, group = sim, color = chain), alpha = 0.8, size = 0.8) +
-    geom_line(data = truecurve, aes(x = time, y = infxns), color = "#000000", linetype = "dashed", size = 1.1) +
-    scale_color_viridis_d("Chain") +
-    labs(caption = "Viridis is inferred, Grey Line is Simulated Infection Curve") +
-    xlab("Time") + ylab("Num. Infxns") +
-    xyaxis_plot_theme
+                                                               by_strata = FALSE)$curvedata %>%
+    dplyr::left_join(., truecurve, by = "time") # note this will be serially duplicated
+  return(infxncurve)
 }
 
 
@@ -56,4 +48,67 @@ serorev_paths <- tibble::tibble(sim = gsub("_SeroRev.RDS", "", basename(serorev_
 #.....................
 # come together
 #......................
-datmap <- dplyr::bind_rows()
+datmap <- dplyr::bind_rows(noserorev_paths, serorev_paths)
+datmap$infxncurve <- purrr::pmap(datmap[, c("curve", "modout")], get_sim_IFR_curve)
+
+#.....................
+# plot out
+#......................
+collated_infxn_curve_NOserorev_plotObj <- datmap %>%
+  dplyr::filter(!serorev_accounted) %>% # no seroreversion first
+  dplyr::select(-c("sim")) %>%
+  dplyr::mutate(simtype = purrr::map_chr(nm, function(x){ switch(x,
+                                                              "expgrowth" = {"Exp. Growth"},
+                                                              "intervene" = {"Outbreak Control"},
+                                                              "secondwave" = {"Second Wave"})})) %>%
+  tidyr::unnest(cols = "infxncurve") %>%
+  dplyr::select(c("simtype", "sens", "spec", "sim", "chain", "time", "totinfxns", "truecurve")) %>%
+  dplyr::mutate(serotestchar = paste0("Sens: ", sens, " ; Spec: ", spec)) %>%
+  ggplot() +
+  geom_line(aes(x = time, y = totinfxns, group = sim, color = chain), alpha = 0.8, size = 0.8) +
+  geom_line(aes(x = time, y = truecurve), color = "#bdbdbd",
+            linetype = "dashed", size = 1) +
+  facet_grid(simtype ~ serotestchar, scales = "free") +
+  scale_color_viridis_d("Chain") +
+  labs(caption = "Viridis is inferred, Grey Line is Simulated Infection Curve") +
+  xlab("Time") + ylab("Num. Infxns") +
+  xyaxis_plot_theme +
+  theme(legend.position = "bottom",
+        strip.text = element_text(size = 11, face = "bold"))
+
+# with serorev
+collated_infxn_curve_serorev_plotObj <- datmap %>%
+  dplyr::filter(serorev_accounted) %>% # no seroreversion first
+  dplyr::select(-c("sim")) %>%
+  dplyr::mutate(simtype = purrr::map_chr(nm, function(x){ switch(x,
+                                                                 "expgrowth" = {"Exp. Growth"},
+                                                                 "intervene" = {"Outbreak Control"},
+                                                                 "secondwave" = {"Second Wave"})})) %>%
+  tidyr::unnest(cols = "infxncurve") %>%
+  dplyr::select(c("simtype", "sens", "spec", "sim", "chain", "time", "totinfxns", "truecurve")) %>%
+  dplyr::mutate(serotestchar = paste0("Sens: ", sens, " ; Spec: ", spec)) %>%
+  ggplot() +
+  geom_line(aes(x = time, y = totinfxns, group = sim, color = chain), alpha = 0.8, size = 0.8) +
+  geom_line(aes(x = time, y = truecurve), color = "#bdbdbd",
+            linetype = "dashed", size = 1) +
+  facet_grid(simtype ~ serotestchar, scales = "free") +
+  scale_color_viridis_d("Chain") +
+  labs(caption = "Viridis is inferred, Grey Line is Simulated Infection Curve") +
+  xlab("Time") + ylab("Num. Infxns") +
+  xyaxis_plot_theme +
+  theme(legend.position = "bottom",
+        strip.text = element_text(size = 11, face = "bold"))
+
+# out
+jpeg("figures/final_figures/collated_simulation_noserorev.jpg",
+     width = 10, height = 7, units = "in", res = 600)
+plot(collated_infxn_curve_NOserorev_plotObj)
+graphics.off()
+
+jpeg("figures/final_figures/collated_simulation_withserorev.jpg",
+     width = 10, height = 7, units = "in", res = 600)
+plot(collated_infxn_curve_serorev_plotObj)
+graphics.off()
+
+
+
