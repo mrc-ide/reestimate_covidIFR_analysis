bf0770ccb5cc0b695008f2667c9a449a7576b89a
nickbrazeau <nbrazeau1@gmail.com>
2020-10-01 11:55:00 -0400

    update delta method

--- R/delta_method.R
+++ R/delta_method.R
@@ -6,33 +6,23 @@
 # we say IFR = g(p) = D/(Np + D), where D = deaths, p = seroprevalence, N = population size
 # by the Delta method we can calculate the Var(g(p)) = [g'(p)]^2 * Var(p), where g'(p) is the first derivative
 # Thus,
-# Var(g(p)) = [-(D * N) / (Np + D)^2]^2 * Var(p) = [D^2 N^2]/[(Np + D)^4] * Var(p)
+# Var(g(p)) = [-(DN) / (Np + D)^2]^2 * Var(p) = ()
 # We can then calculate the 95% confidence intervals as
-# IFR +- 1.96 * (D * N) / (Np + D)^2 * SE(p)
+# IFR +- 1.96 * (DN) / (Np + D)^2 * SE(p)
 
 get_delta_CI_vals <- function(deaths, seroprev, popN, SE, tol) {
-  # basic transforms
-  logit <- function(x, tol=1e-4){
-    return( log(((x+tol)/(1-x+tol))) )
-  }
 
-  expit <- function(x, tol=1e-4){
-    return( 1/(1+exp(-x + tol)) )
-  }
   # calculate critical value from delta method
-  crit_value <- 1.96 * ((deaths * popN) / (seroprev * popN)^2) * SE
+  crit_value <- 1.96 * ((popN * deaths) / (seroprev * popN + deaths)^2) * SE
 
-  # IFR
+  # IFR and cis
   IFRcalc <- deaths  / (seroprev * popN + deaths)
-
-  # do a normal approximation of the binomial CIs
-  LL <- expit( logit(IFRcalc, tol=tol) - crit_value, tol = tol)
-  UL <- expit( logit(IFRcalc, tol=tol) + crit_value, tol = tol)
+  LL <- IFRcalc - crit_value
+  UL <- IFRcalc + crit_value
 
   # out
   ret <- c(LL, UL)
   names(ret) <- c("lower.ci", "upper.ci")
   return(ret)
 }
-
 
