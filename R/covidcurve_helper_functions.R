9d4ae2f7e5c56e5751a3f469bab4ef2244eb5e0d
nickbrazeau <nbrazeau1@gmail.com>
2020-10-17 13:32:22 -0400

    fix ageband 5 yrs

--- R/covidcurve_helper_functions.R
+++ R/covidcurve_helper_functions.R
@@ -12,7 +12,7 @@ get_mid_age <- function(ageband) {
     lwr <- as.numeric(stringr::str_extract(ageband, "[0-9]+(?=\\,)")) + 1 # treat (1, as 1-based, so this is a two year old (since they are >1)
     upr <- as.numeric(stringr::str_extract(ageband, "[0-9]+?(?=])")) + 1
     # fix upper
-    upr[upr == 999] <- 100
+    upr[upr == (999+1)] <- 100
     midages <- purrr::map2_dbl(lwr, upr, function(x, y) mean(c(x,y)))
     return(midages)})
   # out
