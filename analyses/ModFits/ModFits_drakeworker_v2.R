eadfe1d5467982173e2d0182061770865940fee3
nickbrazeau <nbrazeau@med.unc.edu>
2020-12-02 13:13:53 -0500

    better bvec coupling concentration for lower rungs

--- analyses/ModFits/ModFits_drakeworker_v2.R
+++ analyses/ModFits/ModFits_drakeworker_v2.R
@@ -3,7 +3,7 @@
 ##
 ## Notes:
 #........................................................................................
-setwd("/proj/ideel/meshnick/users/NickB/Projects/reestimate_covidIFR_analysis")
+setwd("/proj/ideel/meshnick/users/NickB/Projects/GTIvectest/reestimate_covidIFR_analysis")
 library(drake)
 library(parallel)
 library(COVIDCurve)
@@ -314,12 +314,19 @@ NYS_age_mod <- make_noSeroRev_IFR_model_fit(num_mas = 9, maxMa = "ma9",
                                             sens_spec_tbl = sens_spec_tbl, tod_paramsdf = tod_paramsdf)
 
 
+#............................................................
+#---- Rung Vector #----
+#...........................................................
+#......................
+# concentrate the rungs close to the hottest rung/prior
+# Raising here by GTI_pow so we set GTI_pow = 1.0 downstream
+#......................
+bvec <- seq(0, 1, length.out = 50)^15
 
 
 #............................................................
 #---- Come Together #----
 #...........................................................
-bvec <- seq(0, 1, length.out = 50) ^ seq(5, 2.5, length.out = 50)
 
 fit_map <- tibble::tibble(
   name = c("BRA1_age",
@@ -390,7 +397,7 @@ run_MCMC <- function(path) {
                                         burnin = mod$burnin,
                                         samples = mod$samples,
                                         rungs = mod$rungs,
-                                        GTI_pow = 3.0,
+                                        GTI_pow = 1.0,
                                         beta_manual = mod$bvec[[1]],
                                         cluster = cl,
                                         thinning = mod$thinning)
@@ -406,7 +413,7 @@ run_MCMC <- function(path) {
                                         burnin = mod$burnin,
                                         samples = mod$samples,
                                         rungs = mod$rungs,
-                                        GTI_pow = 3.0,
+                                        GTI_pow = 1.0,
                                         beta_manual = mod$bvec[[1]],
                                         cluster = cl,
                                         thinning = mod$thinning)
@@ -415,8 +422,8 @@ run_MCMC <- function(path) {
   gc()
 
   # out
-  dir.create("/proj/ideel/meshnick/users/NickB/Projects/reestimate_covidIFR_analysis/results/Modfits_noserorev/", recursive = TRUE)
-  outpath = paste0("/proj/ideel/meshnick/users/NickB/Projects/reestimate_covidIFR_analysis/results/Modfits_noserorev/",
+  dir.create("/proj/ideel/meshnick/users/NickB/Projects/GTIvectest/reestimate_covidIFR_analysis/results/Modfits_noserorev/", recursive = TRUE)
+  outpath = paste0("/proj/ideel/meshnick/users/NickB/Projects/GTIvectest/reestimate_covidIFR_analysis/results/Modfits_noserorev/",
                    mod$name, "_rung", mod$rungs, "_burn", mod$burnin, "_smpl", mod$samples, "_NoSeroRev.RDS")
   saveRDS(fit, file = outpath)
   return(0)
