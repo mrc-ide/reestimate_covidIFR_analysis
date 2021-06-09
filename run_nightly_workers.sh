c3bab28ea7ec3648e5702af9b264033060646698
nickbrazeau <nbrazeau@med.unc.edu>
2021-06-08 21:15:07 -0400

    exp longer

--- run_nightly_workers.sh
+++ run_nightly_workers.sh
@@ -6,8 +6,8 @@ sbatch -n 1 --time=5-00:00:00 --mem=24g --wrap="Rscript analyses/ModFits/seroRev
 sbatch -n 1 --time=5-00:00:00 --mem=18g --wrap="Rscript analyses/ModFits/CareHomes_drakeworker.R"
 
 # simulation fits
-sbatch -n 1 --time=5-00:00:00 --mem=24g --wrap="Rscript analyses/SimWork/SimCurves_drakeworker_v2.R"
-sbatch -n 1 --time=5-00:00:00 --mem=24g --wrap="Rscript analyses/SimWork/SeroRev_SimCurves_drakeworker_v2.R"
+sbatch -n 1 --time=8-00:00:00 --mem=24g --wrap="Rscript analyses/SimWork/SimCurves_drakeworker_v2.R"
+sbatch -n 1 --time=8-00:00:00 --mem=24g --wrap="Rscript analyses/SimWork/SeroRev_SimCurves_drakeworker_v2.R"
 sbatch -n 1 --time=5-00:00:00 --mem=18g --wrap="Rscript analyses/SimWork/fit_conceptual_figure_delayeffects.R"
 sbatch -n 1 --time=5-00:00:00 --mem=18g --wrap="Rscript analyses/SimWork/fit_seroday_concept.R"
 
